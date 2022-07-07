09f50869b78d4a584d53e03bc8b97d308a41c9ea
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-07-07 18:51:56 -0300

    agregar carrusel y filtro de categoria

--- html/vistas/js/filtro.js
+++ html/vistas/js/filtro.js
@@ -55,24 +55,85 @@ function mostrarTodo() {
   });
 }
 
-function filtrarTerror() {
-  $("#serie5").show();
-  $("#serie1").hide();
-  $("#serie2").hide();
-  $("#serie3").hide();
-  $("#serie4").hide();
-  $("#serie6").hide();
-  $("#pelicula1").hide();
-  $("#pelicula2").hide();
-  $("#pelicula3").hide();
-  $("#pelicula4").hide();
-  $("#pelicula5").hide();
-  $("#pelicula6").hide();
-}
-
 $(document).ready(function () {
   buscador();
   mostrarTodo();
   filtrarSeries();
   filtrarPeliculas();
+
+  $("#categorias").change(function () {
+    let valor = $(this).val();
+    switch (valor) {
+      case "Terror":
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie5").show();
+        $("#serie6").show();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        break;
+      case "Romance":
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "Suspenso":
+        $("#pelicula1").show();
+        $("#pelicula2").hide();
+        $("#pelicula3").show();
+        $("#pelicula4").show();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").show();
+        $("#serie6").hide();
+        break;
+      case "Comedia":
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").show();
+        $("#pelicula6").show();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").show();
+        $("#serie4").show();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "Accion":
+        $("#pelicula1").show();
+        $("#pelicula2").show();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").show();
+        $("#serie1").hide();
+        $("#serie2").show();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").show();
+        break;
+    }
+  });
 });
