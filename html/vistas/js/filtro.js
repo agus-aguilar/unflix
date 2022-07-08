c7d1f4770043f06db028f07fa2229260de5a7fda
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-07-08 19:34:49 -0300

    buscador de series y peliculas

--- html/vistas/js/filtro.js
+++ html/vistas/js/filtro.js
@@ -1,6 +1,176 @@
 function buscador() {
   $("#buscador").keyup(function (e) {
-    console.log(e.target.value);
+    let buscador = $(this).val();
+    switch(buscador){
+      case "batman":
+        $("#pelicula1").show();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "1917":
+        $("#pelicula2").show();
+        $("#pelicula1").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "joker":
+        $("#pelicula3").show();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "parasite":
+        $("#pelicula4").show();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula5").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "shrek 2":
+        $("#pelicula5").show();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula6").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "piratas del caribe":
+        $("#pelicula6").show();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "breaking bad":
+        $("#pelicula6").hide();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie1").show();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+    case "el marginal":
+        $("#pelicula6").hide();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie2").show();
+        $("#serie1").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+    case "friends":
+        $("#pelicula6").hide();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie3").show();
+        $("#serie2").hide();
+        $("#serie1").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+    case "merli":
+        $("#pelicula6").hide();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie4").show();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie1").hide();
+        $("#serie5").hide();
+        $("#serie6").hide();
+        break;
+      case "ozark":
+        $("#pelicula6").hide();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie5").show();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie1").hide();
+        $("#serie6").hide();
+        break;
+      case "the walking dead":
+        $("#pelicula6").hide();
+        $("#pelicula1").hide();
+        $("#pelicula2").hide();
+        $("#pelicula3").hide();
+        $("#pelicula4").hide();
+        $("#pelicula5").hide();
+        $("#serie1").hide();
+        $("#serie2").hide();
+        $("#serie3").hide();
+        $("#serie4").hide();
+        $("#serie5").hide();
+        $("#serie6").show();
+        break;
+    }
   });
 }
 
@@ -12,6 +182,24 @@ function filtrarSeries() {
     $("#pelicula4").hide();
     $("#pelicula5").hide();
     $("#pelicula6").hide();
+    $("#serie1").show();
+    $("#serie2").show();
+    $("#serie3").show();
+    $("#serie4").show();
+    $("#serie5").show();
+    $("#serie6").show();
+  });
+}
+
+
+function mostrarTodo() {
+  $("#home").click(function () {
+    $("#pelicula1").show();
+    $("#pelicula2").show();
+    $("#pelicula3").show();
+    $("#pelicula4").show();
+    $("#pelicula5").show();
+    $("#pelicula6").show();
     $("#serie1").show();
     $("#serie2").show();
     $("#serie3").show();
@@ -35,23 +223,6 @@ function filtrarPeliculas() {
     $("#pelicula4").show();
     $("#pelicula5").show();
     $("#pelicula6").show();
-  });
-}
-
-function mostrarTodo() {
-  $("#home").click(function () {
-    $("#pelicula1").show();
-    $("#pelicula2").show();
-    $("#pelicula3").show();
-    $("#pelicula4").show();
-    $("#pelicula5").show();
-    $("#pelicula6").show();
-    $("#serie1").show();
-    $("#serie2").show();
-    $("#serie3").show();
-    $("#serie4").show();
-    $("#serie5").show();
-    $("#serie6").show();
   });
 }
 
